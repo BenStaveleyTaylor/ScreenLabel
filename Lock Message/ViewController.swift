f50ebc5a20f31330a5a32d9f94f18ae4e738bebf
Ben Staveley-Taylor <ben.staveley-taylor@oracle.com>
2018-11-06 17:58:21 +0000

    Initial Commit

---
+++ Lock Message/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lock Message
+//
+//  Created by Ben Staveley-Taylor on 06/11/2018.
+//  Copyright © 2018 Ben Staveley-Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
